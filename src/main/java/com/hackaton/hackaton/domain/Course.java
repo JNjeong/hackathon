5d0a6a90aaf4c1663eab42d335787cbdfe70acaf
손성민 <alexzzanga7407@gmail.com>
2024-04-27 01:44:34 +0900

    VO 객체 생성

---
+++ src/main/java/com/hackaton/hackaton/domain/Course.java
@@ -0,0 +1,17 @@
+package com.hackaton.hackaton.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+    private long course_id;
+    private String course_name;
+    private long user_id;
+}
